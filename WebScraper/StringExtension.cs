1637ec6ff900bf6d0ca04ed5ac62a8dfca77ff01
admin <admin@DESKTOP-BN0D3J5>
2024-05-06 22:22:25 +0200

    added MakeShorterPath that makes directories shorter

---
+++ WebScraper/StringExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    public static class StringExtension
+    {
+        public static string MakeShorterPath(this string path) {
+            string[] directories = path.Split('\\');
+
+
+            for (int i = 0; i < directories.Length; i++) {
+                if (directories[i].Length > 25)
+                    directories[i] = GenerateRandomDirectoryName(7);
+            }
+
+            string shortenedPath = string.Join("\\", directories);
+            return shortenedPath;
+        }
+
+        static string GenerateRandomDirectoryName(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
