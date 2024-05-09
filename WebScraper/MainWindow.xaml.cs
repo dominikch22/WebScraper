31979f721c7135b82813661017e63cc7476c0fd0
admin <admin@DESKTOP-BN0D3J5>
2024-05-09 10:39:39 +0200

    add try

--- WebScraper/MainWindow.xaml.cs
+++ WebScraper/MainWindow.xaml.cs
@@ -1,23 +1,7 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Policy;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WebScraper
 {
@@ -32,12 +16,12 @@ namespace WebScraper
             MainBinding = new MainBinding();
 
             DataContext = MainBinding;
-            
+
             BindingOperations.EnableCollectionSynchronization(MainBinding.FileBindings, MainBinding._locker);
 
 
-            List<string> urls = new List<string> { "https://akademiabialska.pl"};
-            
+            List<string> urls = new List<string> { "https://akademiabialska.pl" };
+
             MainBinding.Urls = string.Join("\r\n\r\n", urls);
             MainBinding.Domain = "http://kott.pl";
 
@@ -47,24 +31,27 @@ namespace WebScraper
 
         }
 
-        public void StartClicked(object sender, RoutedEventArgs e) {
-            try {
+        public void StartClicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
                 MainBinding.Running = false;
                 DownloadService.StopDownloading();
 
-                
-                    DownloadService = new DownloadService(MainBinding);
 
-                    MainBinding.FileBindings.Clear(); 
-                    DownloadService.Start();
-                    MainBinding.Running = true;
+                DownloadService = new DownloadService(MainBinding);
 
-                    LocalHttpServer.Start(PathOperation.GetFolderFromDomain(MainBinding.Domain));
+                MainBinding.FileBindings.Clear();
+                DownloadService.Start();
+                MainBinding.Running = true;
 
-                    MainBinding.Error = "";
-                
+                LocalHttpServer.Start(PathOperation.GetFolderFromDomain(MainBinding.Domain));
 
-            }catch (Exception ex)
+                MainBinding.Error = "";
+
+
+            }
+            catch (Exception ex)
             {
                 MainBinding.Error = ex.Message;
                 MainBinding.Running = false;
@@ -83,14 +70,22 @@ namespace WebScraper
 
         public void ContinueClicked(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                MainBinding.Running = false;
+                DownloadService.StopDownloading();
 
-            MainBinding.Running = false;
-            DownloadService.StopDownloading();
-           
                 DownloadService = new DownloadService(MainBinding);
                 DownloadService.Start();
                 MainBinding.Running = true;
-            
+            }
+            catch (Exception ex)
+            {
+                MainBinding.Error = ex.Message;
+                MainBinding.Running = false;
+            }
+
+
         }
 
 
