c8821b9ae5df976b89fdc7748d587ba4b62da908
admin <admin@DESKTOP-BN0D3J5>
2024-05-09 09:13:07 +0200

    removed useless function

--- WebScraper/PathOperation.cs
+++ WebScraper/PathOperation.cs
@@ -153,18 +153,6 @@ namespace WebScraper
         }
 
 
-        static string GenerateRandomDirectoryName(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            Random random = new Random();
-            StringBuilder sb = new StringBuilder(length);
-
-            for (int i = 0; i < length; i++)
-            {
-                sb.Append(chars[random.Next(chars.Length)]);
-            }
-
-            return sb.ToString();
-        }
+        
     }
 }
